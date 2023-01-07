7c02fcac9b9c430aaaae2012f7bf4a518a61741c
Tuukka Veteli <TuukkaVeteli@gmail.com>
2023-01-07 13:26:44 +0200

    created layout wrapper, added footer and header to layout

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,21 +1,26 @@
 import Head from 'next/head';
 import React from 'react';
+import Footer from './footer';
+import Header from './header';
 
 type LayoutProps = {
   children: React.ReactNode,
 };
 
-export default function Layout({ children, }: LayoutProps,) {
+export default function Layout({ children }: LayoutProps) {
   return (
     <>
       <Head>
         <title>Tuukka Veteli</title>
         <meta name="description" content="My over engineered personal website" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
       </Head>
+      <Header />
       <main>
         <div>{children}</div>
       </main>
+      <Footer/>
     </>
   );
 }
